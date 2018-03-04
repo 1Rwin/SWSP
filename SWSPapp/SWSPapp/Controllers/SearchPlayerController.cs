b5f39113f1c82a3e0ed22b7451b3b779ec70665a
1Rwin <paradowski.filip@gmail.com>
2018-03-04 16:27:08 +0100

    comit f

--- SWSPapp/SWSPapp/Controllers/SearchPlayerController.cs
+++ SWSPapp/SWSPapp/Controllers/SearchPlayerController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SWSPapp.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,9 +16,40 @@ namespace SWSPapp.Controllers
             return View();
         }
 
-        public ActionResult SearchPlayer()
+        public ActionResult SearchPlayer(int page=1, string sort = "Name", string sortdir="asc", string search="")
         {
-            return View();
+            int pagesize = 10;
+            int totalrecord = 0;
+            if (page < 1) page = 1;
+            int skip = (page + pagesize) - pagesize;
+            var data = GetPalyerStats(search, sort, sortdir, skip, pagesize, out totalrecord);
+            return View(data);
+        }
+
+        public List<SearchPlayerController> GetPalyerStats(string search, string sort, string sortdir, int skip, int pagesize, out int totalRecord)
+        {
+            using (SWSPContext db = new SWSPContext())
+            {
+                var v = (from a in db.players_statistics
+                         where
+                         a.attack.ToString().Contains(search) ||
+                         a.speed.ToString().Contains(search) ||
+                         a.weight.ToString().Contains(search) ||
+                         a.height.ToString().Contains(search) ||
+                         a.dribble.ToString().Contains(search) ||
+                         a.passing.ToString().Contains(search) ||
+                         a.defense.ToString().Contains(search) ||
+                         a.endurance.ToString().Contains(search)
+
+                         select a);
+                totalRecord = v.Count();
+                v = v.OrderBy(sort + "" + sortdir);
+                if (pagesize >0)
+                {
+                    v = v.Skip(skip).Take(pagesize);
+                }
+                return v.ToList();
+            }
         }
     }
 }
